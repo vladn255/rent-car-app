5dcc8f25273d7c20149c27472a915d1da48626df
vladn255 <nesvitvlad@yandex.ru>
2021-07-17 16:23:59 +0400

    V-06/F-02 - checkout markup
    - added transition to order checkout page

---
+++ src/components/checkout/checkout.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { RoutePath } from "../../const.js";
+
+
+import MainHeader from "../main-header/main-header";
+import OrderReceipt from "../order-receipt/order-receipt";
+
+const Checkout = () => {
+    return (
+        <main className="order-page checkout">
+            <h2 className="visually-hidden">Страница подтвержденного заказа</h2>
+            <MainHeader additionalStyleName={"order-page__header"} />
+            <section className="order-page__content">
+                <div className="order-page__nav">
+                    <span className="order-nav__item checkout__order-info">Заказ номер RU58491823</span>
+                </div>
+                <div className="order-page__form">
+                    <div className="order-page__form-wrapper result__container">
+                        <div className="result__wrapper">
+                            <h3 className="checkout__title">Ваш заказ подтверждён</h3>
+                            <p className="result__model">Hyndai, i30 N</p>
+                            <p className="result__plate-number">K 761 HA 73</p>
+                            <p className="result__additional"><b>Топливо</b> 100%</p>
+                            <p className="result__additional"><b>Доступна с</b> 12.06.2019 12:00</p>
+                        </div>
+                        {/* eslint-disable-next-line no-undef */}
+                        <img className="result__picture" src={`${process.env.PUBLIC_URL}/img/gallery/image2.png`}
+                            alt="ELANTRA view" width="256" height="116"></img>
+                    </div>
+                    <div className="order-page__receipt-wrapper">
+                        <OrderReceipt />
+                        <Link to={RoutePath.ORDER} className="button button--submit button--checkout-color">Отменить</Link>
+                    </div>
+                </div>
+
+            </section >
+        </main >
+    )
+}
+
+export default Checkout;
--- src/components/result-form/result-form.jsx
+++ src/components/result-form/result-form.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable no-undef */
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { RoutePath } from "../../const.js";
@@ -7,6 +6,13 @@ import { RoutePath } from "../../const.js";
 import OrderReceipt from "../order-receipt/order-receipt.jsx";
 
 const ResultForm = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const modalButtonClickHandler = (evt) => {
+        evt.preventDefault();
+        setIsModalOpen(!isModalOpen);
+    }
+
     return (
         <div className="order-page__form result">
             <div className="order-page__form-wrapper result__container">
@@ -17,17 +23,23 @@ const ResultForm = () => {
                     <p className="result__additional"><b>Топливо</b> 100%</p>
                     <p className="result__additional"><b>Доступна с</b> 12.06.2019 12:00</p>
                 </div>
+                {/* eslint-disable-next-line no-undef */}
                 <img className="result__picture" src={`${process.env.PUBLIC_URL}/img/gallery/image2.png`}
                     alt="ELANTRA view" width="256" height="116"></img>
             </div>
 
             <div className="order-page__receipt-wrapper">
                 <OrderReceipt />
-                <button className="button button--submit" type="button">Заказать</button>
+                <button className="button button--submit" type="button" onClick={modalButtonClickHandler}>Заказать</button>
             </div>
 
-            <div className="modal modal--closed">
-                <Link to={RoutePath.CHECKOUT} className="button button--submit">Подтвердить</Link>
+            <div className={`modal ${isModalOpen ? `` : `modal--closed`}`}>
+                <div className="modal__wrapper">
+                    <h3 className="modal__title">Подтвердить заказ</h3>
+                    <Link to={RoutePath.CHECKOUT} className="button button--modal-link">Подтвердить</Link>
+                    <button className="button button--modal-close button--checkout-color" type="button" onClick={modalButtonClickHandler}>Вернуться</button>
+                </div>
+
             </div>
         </div>
     )
